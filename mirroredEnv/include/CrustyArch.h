ab48331c2a3b81597d7948bdef873b7057d13cdb
Silverion <55493559+shahern004@users.noreply.github.com>
2025-04-10 11:19:47 -0600

    Create CrustyArch.h

---
+++ mirroredEnv/include/CrustyArch.h
@@ -0,0 +1,14 @@
+#ifndef CRUSTY_ARCHITECTURE_EXAMPLE_H
+#define CRUSTY_ARCHITECTURE_EXAMPLE_H
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * Simple test function to verify FFI boundary is working
+ */
+uint32_t rust_function_blank(void);
+
+#endif  /* CRUSTY_ARCHITECTURE_EXAMPLE_H */
